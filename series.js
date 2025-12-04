cf557c7ce6a3686f015c7071a745108001fb935f
Satyam Singh <m4740600@gmail.com>
2025-12-04 09:36:03 +0530

    Update series.js

--- series.js
+++ series.js
@@ -75,7 +75,8 @@ const seriesDB = {
                         duration: '46m',
                         description: 'A frantic Jonathan looks for Nancy in the darkness, but Steve\'s looking for her, too. Hopper and Joyce uncover the truth about the lab\'s experiments.',
                         thumbnail: 'S01E06.png',
-                        videoUrl: 'https://drive.google.com/file/d/1OVETghmdspFiIOI5Uqn-9in_aqu7vkxr/preview',
+                        // videoUrl: 'https://drive.google.com/file/d/1OVETghmdspFiIOI5Uqn-9in_aqu7vkxr/preview',
+                        videoUrl:'https://drive.google.com/file/d/1IsUbSHqcBXb7FchWm7TeDfHsCr06RErI/view?usp=drive_link',
                         releaseDate: 'July 15, 2016'
                     },
                     {
@@ -310,6 +311,7 @@ if (typeof window !== 'undefined') {
     window.playSeriesFromStart = playSeriesFromStart;
     window.loadSeriesContent = loadSeriesContent;
 }
+
 
 
 
